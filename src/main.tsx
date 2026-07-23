25f961965b865b9885ebf026ed493489773f7a59
shady nasif <shady.nasif@gmail.com>
2026-07-24 00:33:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { installAuthFetch } from './lib/authFetch.ts';
+
+installAuthFetch();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
